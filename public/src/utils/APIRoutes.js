a5cc8b49d67181f7c4342afa9061d5cefa34bf0f
Ridham Patel <105585070+ridhampatel24@users.noreply.github.com>
2024-05-02 11:04:52 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://backend.ridhampatel.tech";
+export const host = "http://3.108.215.161:5000";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
